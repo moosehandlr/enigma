9b9f2c533d5ae57c6949e8b0a3c7b315f915c8d5
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-06 12:28:48 -0600

    Create gen_offsets and create offsets hash.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,5 @@
 class Enigma
-  attr_reader :keys
+  attr_reader :keys, :offsets
  def initialize
    @keys = Hash.new
    @offsets = Hash.new
@@ -8,14 +8,19 @@ class Enigma
  def gen_keys
    random = 5.times.map{rand(0..9)}
    key_nums = random.each_cons(2).to_a
-   @keys[:akey] = key_nums[0].join.to_i
-   @keys[:bkey] = key_nums[1].join.to_i
-   @keys[:ckey] = key_nums[2].join.to_i
-   @keys[:dkey] = key_nums[3].join.to_i
+   @keys[:a_key] = key_nums[0].join.to_i
+   @keys[:b_key] = key_nums[1].join.to_i
+   @keys[:c_key] = key_nums[2].join.to_i
+   @keys[:d_key] = key_nums[3].join.to_i
  end
 
  def gen_offsets(date)
-
+   date_squared = date.to_i**2
+   offset_nums = date_squared.to_s.split(//).last(4)
+   @offsets[:a_offset] = offset_nums[0].to_i
+   @offsets[:b_offset] = offset_nums[1].to_i
+   @offsets[:c_offset] = offset_nums[2].to_i
+   @offsets[:d_offset] = offset_nums[3].to_i
  end
 
 
