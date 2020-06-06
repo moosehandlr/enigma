3b9b3899c5a7ac9a759ac0b77084fca8e4888e1e
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-06 12:01:19 -0600

    Create gen_keys. Test it can generate random keys and create a hash.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,19 @@
 class Enigma
+  attr_reader :keys
  def initialize
+   @keys = Hash.new
+   @offsets = Hash.new
  end
 
- 
+ def gen_keys
+   random = 5.times.map{rand(0..9)}
+   key_nums = random.each_cons(2).to_a
+   @keys[:akey] = key_nums[0].join.to_i
+   @keys[:bkey] = key_nums[1].join.to_i
+   @keys[:ckey] = key_nums[2].join.to_i
+   @keys[:dkey] = key_nums[3].join.to_i
+ end
+
+
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,9 +8,11 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
-  def test_it_can_create_keys
-    expected = {"A Key" => 02,"B Key" => 27, "C Key" => 71, "D Key" => 15}
-    assert_equal expected, enigma.keys
+  def test_it_can_generate_keys
+    enigma = Enigma.new
+    enigma.gen_keys
+    assert hash, enigma.keys
+    p enigma.keys
   end
 
 end
