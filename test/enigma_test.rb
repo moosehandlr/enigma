6444e174eea88dfbde5e8199e26d5c69d1bad3b9
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-06 11:13:58 -0600

    Create enigma class and test is exists.

---
+++ test/enigma_test.rb
@@ -0,0 +1,14 @@
+require './test/test_helper'
+require'./lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def test_it_exists
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
+  end
+
+  #def test_it_has_attributes
+  #end
+
+end
