dd72e2402f88472f4f0fd76bafa7b1765d2b7a3d
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-07 10:53:14 -0600

    Define gen offsets method.

--- lib/offsets.rb
+++ lib/offsets.rb
@@ -1,4 +1,15 @@
 class Offsets
+  def initialize
+  end
 
-
+  def gen_offsets(date)
+    offsets = {}
+    date_squared = date.to_i**2
+    offset_nums = date_squared.to_s.split(//).last(4)
+    offsets[:a_offset] = offset_nums[0].to_i
+    offsets[:b_offset] = offset_nums[1].to_i
+    offsets[:c_offset] = offset_nums[2].to_i
+    offsets[:d_offset] = offset_nums[3].to_i
+    offsets
+  end
 end
