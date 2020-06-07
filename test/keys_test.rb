128df6cdd80c8b5a0fc39ba008d25ca300a3dcbe
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-06 20:51:49 -0600

    Test it can generate keys when given 5 digit number.

---
+++ test/keys_test.rb
@@ -0,0 +1,16 @@
+require './test/test_helper'
+require './lib/keys'
+
+class KeysTest < Minitest::Test
+
+  def test_it_exists
+    keys = Keys.new
+    assert_instance_of Keys, keys
+  end
+
+  def test_it_can_generate_keys_with_argument
+    keys = Keys.new
+    assert_equal [["0","2"],["2","7"],["7","1"],["1","5"]],keys.gen_keys("02715")
+  end
+
+end
