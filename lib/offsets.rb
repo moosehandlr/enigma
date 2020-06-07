56806cf1d2709d721480729c9a4a31f94bdf7443
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-07 14:00:46 -0600

    Refactor gen today method to return string with todays date in mmddyy format.

--- lib/offsets.rb
+++ lib/offsets.rb
@@ -13,16 +13,9 @@ class Offsets
   end
 
   def self.gen_today
-    offsets = {}
     todays_date =  Date.today.strftime("%d%m%Y").chars
     todays_date.delete_at(4)
     todays_date.delete_at(4)
-    date_squared = todays_date.join.to_i**2
-    offset_nums = date_squared.to_s.split(//).last(4)
-    offsets[:a_offset] = offset_nums[0].to_i
-    offsets[:b_offset] = offset_nums[1].to_i
-    offsets[:c_offset] = offset_nums[2].to_i
-    offsets[:d_offset] = offset_nums[3].to_i
-    offsets
+    todays_date.join
   end
 end
