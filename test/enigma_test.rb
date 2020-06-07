d6dbdb67edea0d8ca49011518c1dd43a54e19823
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-07 12:10:56 -0600

    Remove tests for gen shift.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,16 +8,6 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
-  def test_it_can_generate_shifts
-    enigma = Enigma.new
-    enigma.gen_keys("02715")
-    enigma.gen_offsets("040895")
-    enigma.gen_shifts
-
-    expected = {a_shift: 3, b_shift: 27, c_shift: 73, d_shift: 20}
-    assert_equal expected, enigma.shifts
-  end
-
   def test_it_can_encrypt_message
     enigma = Enigma.new
     enigma.gen_keys("02715")
