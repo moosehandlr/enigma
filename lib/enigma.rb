5d428017344baf4b3bf3c51e40e6a972a3c80b01
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-07 12:30:06 -0600

    Require keys/offsets/shifts classes.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,10 @@
 require 'date'
+require './lib/keys'
+require './lib/offsets'
+require './lib/shifts'
 
 class Enigma
-  attr_reader :offsets, :shifts, :alphabet, :encryption
+  attr_reader :alphabet, :encryption
  def initialize
    @alphabet = ("a".."z").to_a << " "
    @encryption = Hash.new
@@ -11,7 +14,7 @@ class Enigma
    encrypted_message = []
    split_message = message.chars.each_slice(4).to_a
    split_message.each do |split|
-     Hash[split.map.with_index { |x, i| [i, x] }].each do |k,v|
+     Hash[split.map.with_index {|x, i| [i, x]}].each do |k,v|
        if k == 0
          encrypted_message << @alphabet.rotate(@shifts[:a_shift])[@alphabet.index(split[0].downcase)]
        elsif k == 1
