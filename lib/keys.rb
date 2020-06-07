c44fc85058953d8a703507f6e2bd3f16aa3a6759
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-07 14:21:56 -0600

    Refactor gen_random class method to return string.

--- lib/keys.rb
+++ lib/keys.rb
@@ -11,12 +11,6 @@ class Keys
   end
 
   def self.gen_random
-    keys = {}
-    paired_keys = 5.times.map{rand(0..9)}.each_cons(2).to_a
-    keys[:a_key] = paired_keys[0].join.to_i
-    keys[:b_key] = paired_keys[1].join.to_i
-    keys[:c_key] = paired_keys[2].join.to_i
-    keys[:d_key] = paired_keys[3].join.to_i
-    keys
+    5.times.map{rand(0..9)}.join
   end
 end
