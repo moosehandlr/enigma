0bc5e3d9dc5707aaf12c816c189c6bcfe4160fa5
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-06 22:49:37 -0600

    Refactor test to show hash of known keys and random keys.

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -4,18 +4,21 @@ require './lib/keys'
 class KeysTest < Minitest::Test
 
   def test_it_exists
-    keys = Keys.new
-    assert_instance_of Keys, keys
+    key = Keys.new
+    assert_instance_of Keys, key
   end
 
-  def test_it_can_generate_keys_with_argument
-    keys = Keys.new
-    assert_equal [["0","2"],["2","7"],["7","1"],["1","5"]],keys.gen_keys("02715")
+  def test_it_can_generate_keys_with_known_num
+    key = Keys.new
+    key.gen_keys("02715")
+    expected = {a_key: 02, b_key: 27, c_key: 71, d_key: 15}
+    assert_equal expected, key.keys
   end
 
   def test_it_can_generate_random_keys
-    keys = Keys.new
-    assert_not_equal [["0","2"],["2","7"],["7","1"],["1","5"]],keys.gen_random
+    key = Keys.new
+    key.gen_random
+    expected = {a_key: 71, b_key: 03, c_key: 16, d_key: 22}
+    assert_equal expected, key.keys
   end
-
 end
