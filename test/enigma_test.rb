11dcc0521c7fbf9a5becd643dc3fa8bad25d4670
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-07 11:16:50 -0600

    Delete test for keys and offsets from enigma test.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,20 +8,6 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
-  def test_it_can_generate_keys_from_given_num
-    enigma = Enigma.new
-    enigma.gen_keys("02715")
-    expected = {a_key: 02, b_key: 27, c_key: 71, d_key: 15}
-    assert_equal expected, enigma.keys
-  end
-
-  def test_it_can_generate_date_offsets
-    enigma = Enigma.new
-    expected = {a_offset: 1, b_offset: 0, c_offset: 2, d_offset: 5}
-    enigma.gen_offsets("040895")
-    assert_equal expected, enigma.offsets
-  end
-
   def test_it_can_generate_shifts
     enigma = Enigma.new
     enigma.gen_keys("02715")
