917b42144ad36e8c81ab3defb2af4dd6b94d233b
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-06 22:58:06 -0600

    Correct hash name.

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,22 +1,28 @@
 class Keys
-  attr_reader :keys
   def initialize
-    @keys = Hash.new
   end
 
   def gen_keys(key)
+    keys = {}
     paired_keys = key.split(//).each_cons(2).to_a
-    @keys[:a_key] = paired_keys[0].join.to_i
-    @keys[:b_key] = paired_keys[1].join.to_i
-    @keys[:c_key] = paired_keys[2].join.to_i
-    @keys[:d_key] = paired_keys[3].join.to_i
+    keys[:a_key] = paired_keys[0].join.to_i
+    keys[:b_key] = paired_keys[1].join.to_i
+    keys[:c_key] = paired_keys[2].join.to_i
+    keys[:d_key] = paired_keys[3].join.to_i
+    keys
   end
 
   def gen_random
+    keys = {}
     paired_keys = 5.times.map{rand(0..9)}.each_cons(2).to_a
-    @keys[:a_key] = paired_keys[0].join.to_i
-    @keys[:b_key] = paired_keys[1].join.to_i
-    @keys[:c_key] = paired_keys[2].join.to_i
-    @keys[:d_key] = paired_keys[3].join.to_i
+    keys[:a_key] = paired_keys[0].join.to_i
+    keys[:b_key] = paired_keys[1].join.to_i
+    keys[:c_key] = paired_keys[2].join.to_i
+    keys[:d_key] = paired_keys[3].join.to_i
+    keys
+  end
+
+  def hash_render
+
   end
 end
