cf399247b7edc149604b61df06b8bfd75331e814
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-07 12:07:59 -0600

    Move all keys and offsets test to respective folders. Change test test for class methods.

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -8,16 +8,14 @@ class KeysTest < Minitest::Test
     assert_instance_of Keys, key
   end
 
-  def test_it_can_generate_keys_with_known_num
-    key = Keys.new
+  def test_it_can_generate_keys_with_given_key
     expected = {a_key: 02, b_key: 27, c_key: 71, d_key: 15}
-    assert_equal expected, key.gen_keys("02715")
+    assert_equal expected, Keys.gen_keys("02715")
   end
 
   def test_it_can_generate_random_keys
     skip
-    key = Keys.new
     expected = {a_key: 71, b_key: 03, c_key: 16, d_key: 22}
-    assert_equal expected, key.gen_random
+    assert_equal expected, Keys.gen_random
   end
 end
--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -9,15 +9,12 @@ class OffsetsTest < Minitest::Test
   end
 
   def test_it_can_generate_offsets_with_given_date
-    offset = Offsets.new
     expected = {a_offset: 1, b_offset: 0, c_offset: 2, d_offset: 5}
-    assert_equal expected, offset.gen_offsets("040895")
+    assert_equal expected, Offsets.gen_offsets("040895")
   end
 
   def test_it_can_generate_offsets_with_todays_date
-    offset = Offsets.new
     expected = {a_offset: 4, b_offset: 4, c_offset: 0, d_offset: 0}
-    assert_equal expected, offset.gen_today
+    assert_equal expected, Offsets.gen_today #07/06/20
   end
-
 end
