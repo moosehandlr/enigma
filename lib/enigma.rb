6a04c2c80212174997c45d4dfb1732c19824d2cb
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-07 13:06:52 -0600

    Update initialize methods. Define shifts value within encrypt method scope. Update encrypt code to workin with shift value.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,32 +4,34 @@ require './lib/offsets'
 require './lib/shifts'
 
 class Enigma
-  attr_reader :alphabet, :encryption
+  attr_reader :alphabet
  def initialize
    @alphabet = ("a".."z").to_a << " "
-   @encryption = Hash.new
  end
 
  def encrypt(message, key, date)
+   shifts = Shifts.gen_shifts(key, date)
+   encryption = {}
    encrypted_message = []
-   split_message = message.chars.each_slice(4).to_a
-   split_message.each do |split|
-     Hash[split.map.with_index {|x, i| [i, x]}].each do |k,v|
+
+   parse_message = message.chars.each_slice(4).to_a
+   parse_message.each do |group|
+     Hash[group.map.with_index {|x, i| [i, x]}].each do |k,v|
        if k == 0
-         encrypted_message << @alphabet.rotate(@shifts[:a_shift])[@alphabet.index(split[0].downcase)]
+         encrypted_message << @alphabet.rotate(shifts[:a_shift])[@alphabet.index(split[0].downcase)]
        elsif k == 1
-         encrypted_message << @alphabet.rotate(@shifts[:b_shift])[@alphabet.index(split[1].downcase)]
+         encrypted_message << @alphabet.rotate(shifts[:b_shift])[@alphabet.index(split[1].downcase)]
        elsif k == 2
-         encrypted_message << @alphabet.rotate(@shifts[:c_shift])[@alphabet.index(split[2].downcase)]
+         encrypted_message << @alphabet.rotate(shifts[:c_shift])[@alphabet.index(split[2].downcase)]
        elsif k == 3
-         encrypted_message << @alphabet.rotate(@shifts[:d_shift])[@alphabet.index(split[3].downcase)]
+         encrypted_message << @alphabet.rotate(shifts[:d_shift])[@alphabet.index(split[3].downcase)]
        end
      end
    end
-
-   @encryption[:encryption] = encrypted_message.join
-   @encryption[:key] = key
-   @encryption[:date] = date
+   encryption[:encryption] = encrypted_message.join
+   encryption[:key] = key
+   encryption[:date] = date
+   encryption
  end
 
 end
