b9e11a8aaf27b4107fe13a90a5c7353ec11ae5b0
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-06 22:50:27 -0600

    Refactor to generate hash for known keys and random keys.

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,16 +1,22 @@
 class Keys
+  attr_reader :keys
   def initialize
-
+    @keys = Hash.new
   end
 
   def gen_keys(key)
-    key.split(//).each_cons(2).to_a
+    paired_keys = key.split(//).each_cons(2).to_a
+    @keys[:a_key] = paired_keys[0].join.to_i
+    @keys[:b_key] = paired_keys[1].join.to_i
+    @keys[:c_key] = paired_keys[2].join.to_i
+    @keys[:d_key] = paired_keys[3].join.to_i
   end
 
-    # key_nums = random.
-    # @keys[:a_key] = key_nums[0].join.to_i
-    # @keys[:b_key] = key_nums[1].join.to_i
-    # @keys[:c_key] = key_nums[2].join.to_i
-    # @keys[:d_key] = key_nums[3].join.to_i
-
+  def gen_random
+    paired_keys = 5.times.map{rand(0..9)}.each_cons(2).to_a
+    @keys[:a_key] = paired_keys[0].join.to_i
+    @keys[:b_key] = paired_keys[1].join.to_i
+    @keys[:c_key] = paired_keys[2].join.to_i
+    @keys[:d_key] = paired_keys[3].join.to_i
+  end
 end
