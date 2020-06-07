17c40c4e6a085df65125324dcbf49962820fc40b
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-07 13:09:02 -0600

    Create decryption test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,4 +14,10 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, enigma.encrypt("HELLO WORLD", "02715", "040895")
   end
 
+  def test_it_can_decrypt_ciphertext_and_return_hash
+    enigma = Enigma.new
+    expected = {decryption: "hello world", key: "02715", date: "040895"}
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
 end
