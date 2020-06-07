d59c2901b4ff00fd0ee816e011add7b8889451ce
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-07 12:09:52 -0600

    Test shift can create a hash with shift values.

--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -7,3 +7,10 @@ class ShiftsTest < Minitest::Test
     shift = Shifts.new
     assert_instance_of Shifts, shift
   end
+
+  def test_it_can_generate_shift_with_given_key_and_date
+    expected = {a_shift: 3, b_shift: 27, c_shift: 73, d_shift: 20}
+    assert_equal expected, Shifts.gen_shifts("02715", "040895")
+  end
+
+end
