b02ef0598abff078169f70a7d9524dddbca6fd96
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-08 14:36:39 -0600

    Create buffer method to add leading zeros to less than 5 digit numbers.

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,8 +1,9 @@
 class Keys
-  
+
   def self.gen_keys(key)
     keys = {}
-    paired_keys = key.split(//).each_cons(2).to_a
+    key_buff = key.rjust(5,'0')
+    paired_keys = key_buff.split(//).each_cons(2).to_a
     keys[:a_key] = paired_keys[0].join.to_i
     keys[:b_key] = paired_keys[1].join.to_i
     keys[:c_key] = paired_keys[2].join.to_i
